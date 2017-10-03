5b485698e4bb04c006dca1c41553a1bfe49a56b0
Coquenim <nelson_coquenim@hotmail.com>
2017-10-03 13:54:38 +0100

    Minor tweak to test script

--- src/test.bash
+++ src/test.bash
@@ -1,1 +1,6 @@
+echo number of threads = 2 - 10 times
 seq 10 | xargs -Iz ./othello -c -t -d 8 -b 10 -s -n 2
+
+echo number of threads = 4 - 10 times
+
+seq 10 | xargs -Iz ./othello -c -t -d 8 -b 10 -s -n 4
