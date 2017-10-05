429b8445d6dda850a842a7c8c8f883c6c65f4dec
Coquenim <nelson_coquenim@hotmail.com>
2017-10-05 01:01:37 +0100

    Improvements on the testing script

--- src/test.bash
+++ src/test.bash
@@ -1,18 +1,21 @@
-echo number of threads = 2 - 10 times board 10
-seq 10 | xargs -Iz ./othello -c -t -b 10 -s -n 2
+echo "threads = 1 | 10 times | board = 10" | tee -a resultados.txt 
+seq 10 | xargs -Iz ./othello -c -t -b 10 -s -n 1 | tee -a resultados.txt
+echo | tee -a resultados.txt
+echo "threads = 2 | 10 times | board = 10" | tee -a resultados.txt
+seq 10 | xargs -Iz ./othello -c -t -b 10 -s -n 2 | tee -a resultados.txt
+echo | tee -a resultados.txt
 
-echo number of threads = 1 - 10 times board 10
-seq 10 | xargs -Iz ./othello -c -t -b 10 -s -n 1
+echo "threads = 1 | 10 times | board = 100" | tee -a resultados.txt
+seq 10 | xargs -Iz ./othello -c -t -b 100 -s -n 1 | tee -a resultados.txt
+echo | tee -a resultados.txt
+echo "threads = 2 | 10 times | board = 100" | tee -a resultados.txt
+seq 10 | xargs -Iz ./othello -c -t -b 100 -s -n 2 | tee -a resultados.txt
+echo | tee -a resultados.txt
 
 
-echo number of threads = 2 - 10 times board 100
-seq 10 | xargs -Iz ./othello -c -t -b 100 -s -n 2
-
-echo number of threads = 1 - 10 times board 100
-seq 10 | xargs -Iz ./othello -c -t -b 100 -s -n 1
-
-echo number of threads = 2 - 10 times board 1000
-seq 10 | xargs -Iz ./othello -c -t -b 1000 -s -n 2
-
-echo number of threads = 1 - 10 times board 1000
-seq 10 | xargs -Iz ./othello -c -t -b 1000 -s -n 1
+echo "threads = 1 | 10 times | board = 200" | tee -a resultados.txt
+seq 10 | xargs -Iz ./othello -c -t -b 200 -s -n 1 | tee -a resultados.txt
+echo | tee -a resultados.txt
+echo "threads = 2 | 10 times | board = 200" | tee -a resultados.txt
+seq 10 | xargs -Iz ./othello -c -t -b 200 -s -n 2 |tee -a resultados.txt
+echo | tee -a resultados.txt
