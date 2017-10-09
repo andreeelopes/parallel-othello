3da32d9067731b0d1a9bdde0af3bddcb2ddd4074
Coquenim <nelson_coquenim@hotmail.com>
2017-10-09 22:40:45 +0100

    Minor tweak to test script

--- src/test_2.sh
+++ src/test_2.sh
@@ -3,9 +3,9 @@ rm results.txt
 
 for b in 10 25 50 100 150
 do
-    for n in 1 2 4 8 16 32
+    for n in 1 2 4 8 10 12 14 16
     do
-        for i in 1 2 3 4 5
+        for i in 1 2 3 4 5 6 7 9 10
         do
             ./othello -s -t -b $b -n $n >> results.txt
         done
